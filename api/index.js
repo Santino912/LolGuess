cd12890e5ce904eb175894370c811231cddf939a
Santino <santinoscolamieri@gmail.com>
2023-11-07 19:16:20 -0300

    Corrections to deploy

--- api/index.js
+++ api/index.js
@@ -1,7 +1,10 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import router from "./routes/index.js";
+import getChampWithNickname from "./Controllers/getChampWithNickname.js";
+import getChampSplashart from "./Controllers/getChampSplashArt.js";
+import getAllChamps from "./Controllers/getAllChamps.js";
+
 dotenv.config();
 
 const PORT = process.env.PORT || 3001;
@@ -10,7 +13,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use("/", router);
+app.get("/allchamps", getAllChamps);
+app.get("/allchampssplashart", getChampSplashart);
+app.get("/allchampsnickname", getChampWithNickname);
 
 app.listen(PORT, () => {
   console.log(`Server Started in ${PORT}`);
