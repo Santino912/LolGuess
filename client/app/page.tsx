909c0cbf7b13f613e76a66b77e575a418a50cdb0
Santino <santinoscolamieri@gmail.com>
2023-11-10 07:25:30 -0300

    Corrections to deploy

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -129,7 +129,7 @@ export default function Home() {
                   />
                 </Box>
               )}
-            {loadingState && answer?.id && (
+            {!answer?.id && (
               <Box
                 display={"flex"}
                 alignItems={"center"}
